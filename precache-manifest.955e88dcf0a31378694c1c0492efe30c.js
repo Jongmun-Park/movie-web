23e45ef68b17743e6d040aa3276b21214b7633bf
Jongmun-Park <momenttraveler@gmail.com>
2019-08-20 11:54:17 +0900

    Updates

---
+++ precache-manifest.955e88dcf0a31378694c1c0492efe30c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c521eadfd66917a576eea3cfc276b52",
+    "url": "/movie-web/index.html"
+  },
+  {
+    "revision": "bc3b1c2b34f473a4e7da",
+    "url": "/movie-web/static/css/main.5ca98196.chunk.css"
+  },
+  {
+    "revision": "63593a1f74bfb07e977d",
+    "url": "/movie-web/static/js/2.fd1720fa.chunk.js"
+  },
+  {
+    "revision": "bc3b1c2b34f473a4e7da",
+    "url": "/movie-web/static/js/main.ebd51f2e.chunk.js"
+  },
+  {
+    "revision": "a9949924cec6d93b9e05",
+    "url": "/movie-web/static/js/runtime~main.230531f2.js"
+  }
+]);
