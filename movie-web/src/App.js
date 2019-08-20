36402a6b5b3c803d142a20b4767c978f6a3c45d0
Jongmun-Park <momenttraveler@gmail.com>
2019-08-20 11:20:53 +0900

    make Components

--- movie-web/src/App.js
+++ movie-web/src/App.js
@@ -4,49 +4,51 @@ import Movie from './Movie';
 
 
 class App extends Component {
+// Render: componentWillMount() -> render() -> componentDidMount()
+// Update componentWillReceiveProps() -> shouldComponentUpdate() -> componentWillUpdate() -> render() -> componentDidUpdate()
 
-  state = {
-  }
+state = {}
 
-  componentDidMount(){
-    setTimeout(() => {
-      this.setState({
-        movies: [
-          {
-            title: "Guardians of The Galaxy",
-            poster: "http://www.slashfilm.com/wp/wp-content/images/guardians-of-the-galaxy-vol-2-tv-spot.jpg",
-          },
-          {
-            title: "Matrix",
-            poster: "https://boygeniusreport.files.wordpress.com/2017/10/the-matrix.jpg?quality=98&strip=all",
-          },
-          {
-            title: "Oldboy",
-            poster: "https://www.slantmagazine.com/wp-content/uploads/2004/06/oldboy.jpg",
-          },
-          {
-            title: "Into The Wild",
-            poster: "https://experiencefilmsite.files.wordpress.com/2019/05/cover-experiences.jpg?w=736&h=380&crop=1",
-          },
-        ]
-      })
-    }, 5000);
-  }
+componentDidMount(){
+  this._getMovies();
+}
+
+_getMovies = async () => {
+  const movies = await this._callApi()
+  this.setState({
+    movies
+  })
+}
+
+_callApi = () => {
+  return fetch('https://yts.lt/api/v2/list_movies.json?sort_by=download_count')
+  .then(response => response.json())
+  .then(json => json.data.movies)
+  .catch(err => console.log(err))
+}
 
 _renderMovies = () => {
-  const movies = this.state.movies.map((movie, index) => {
-    return <Movie title={movie.title} poster={movie.poster} key={index}></Movie>
+  const movies = this.state.movies.map(movie => {
+    return <Movie
+      title={movie.title_english}
+      poster={movie.medium_cover_image}
+      key={movie.id}
+      genres={movie.genres}
+      synopsis={movie.synopsis}>
+      </Movie>
   })
   return movies
 }
 
   render(){
+    const { movies } = this.state;
     return (
-      <div className="App">
-        {this.state.movies ? this._renderMovies() : 'Loading'}
+      <div className={movies ? "App" : "App--loading"}>
+        {movies ? this._renderMovies() : 'Loading'}
       </div>
     );
   }
+
 }
 
 export default App;
--- movie-web/src/Movie.js
+++ movie-web/src/Movie.js
@@ -1,30 +1,52 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import './Movie.css';
 
-class Movie extends Component{
-
-  static propTypes = {
-    title: PropTypes.string.isRequired,
-    poster: PropTypes.string.isRequired,
-  }
-
-  render(){
-    return(
-      <div>
-        <h2>{this.props.title}</h2>
-        <MoviePoster poster={this.props.poster}/>
+function Movie({title, poster, genres, synopsis}){
+  return (
+      <div className="Movie">
+        <div className="Movie__Columns">
+          <MoviePoster poster={poster} alt={title}/>
+        </div>
+        <div className="Movie_Columns">
+          <h2>{title}</h2>
+          <div className="Movie_Genres">
+            {genres.map((genre, index) => <MovieGenre genre={genre} key={index}></MovieGenre>)}
+          </div>
+          <p className="Movie__Synopsis">
+            {synopsis}
+          </p>
+        </div>
       </div>
-    );
-  }
+  )
 }
 
-class MoviePoster extends Component{
-  render(){
-    return(
-      <img src={this.props.poster} alt="Movie Poster"/>
-    )
-  };
+function MoviePoster({poster, alt}){
+  return (
+    <img src={poster} alt={alt} title={alt} className="Movie__Poster"/>
+  )
+}
+
+function MovieGenre({genre}){
+  return (
+    <span className="Movie__Genre">{genre}</span>
+  )
+}
+
+Movie.protoTypes = {
+  title: PropTypes.string.isRequired,
+  poster: PropTypes.string.isRequired,
+  genres: PropTypes.array.isRequired,
+  synopsis: PropTypes.string.isRequired,
+}
+
+MoviePoster.protoTypes = {
+  poster: PropTypes.string.isRequired,
+  alt: PropTypes.string.isRequired,
+}
+
+MovieGenre.protoTypes = {
+  genre: PropTypes.string.isRequired,
 }
 
 export default Movie
